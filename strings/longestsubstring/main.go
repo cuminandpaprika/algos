7068d354d2bc08d1ef4bfb94d4921137d06034b2
Jack Zheng <cuminandpaprika@github.com>
2024-01-08 16:26:12 +0300

    Add longest substring

---
+++ strings/longestsubstring/main.go
@@ -0,0 +1,25 @@
+package substring
+
+func lengthOfLongestSubstring(s string) int {
+	longestWindow := 0
+	startIndex := 0
+	seenChars := map[rune]int{}
+
+	// dvdf
+	for i, char := range s {
+		if position := seenChars[char]; position > startIndex {
+			// Shift the left side of the window
+			startIndex = position
+		}
+
+		// Keep track of where we need to seek to, to remove the duplicate
+		seenChars[char] = i + 1
+
+		if currentWindow := i - startIndex + 1; currentWindow > longestWindow {
+			longestWindow = currentWindow
+		}
+
+	}
+
+	return longestWindow
+}
